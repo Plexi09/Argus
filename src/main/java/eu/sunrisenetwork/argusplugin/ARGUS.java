634cda459916e3f97b5f97b40deb6f5b631dd246
Plexi09 <pierre.emmanuel.gobillard@gmail.com>
2024-07-07 11:03:11 +0200

    Added the congig loader

--- src/main/java/eu/sunrisenetwork/argusplugin/ARGUS.java
+++ src/main/java/eu/sunrisenetwork/argusplugin/ARGUS.java
@@ -4,10 +4,19 @@ import eu.sunrisenetwork.argusplugin.commands.*;
 import eu.sunrisenetwork.argusplugin.util.MessageUtils;
 import eu.sunrisenetwork.argusplugin.listeners.TopLuckInventoryListener;
 import eu.sunrisenetwork.argusplugin.listeners.InventoryClickListener;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ARGUS extends JavaPlugin {
+    private File customConfigFile;
+    private FileConfiguration customConfig;
     private boolean debug;
 
     @Override
@@ -45,7 +54,8 @@ public class ARGUS extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new TopLuckInventoryListener(), this);
 
         MessageUtils.sendMessage(getServer().getConsoleSender(), "Loading the config... ");
-        loadConfig();
+        createCustomConfig();
+        Boolean time = ARGUS.getConfig().getBoolean("debug");	
 
         if (debug) {
         	MessageUtils.sendMessage(getServer().getConsoleSender(),"Debug mode is enabled.");
@@ -57,16 +67,32 @@ public class ARGUS extends JavaPlugin {
         MessageUtils.sendMessage(getServer().getConsoleSender(), "Plugin started in " + duration + " ms.");
         MessageUtils.sendMessage(getServer().getConsoleSender(), "Thanks you for using Argus !");
     }
+    
+    public FileConfiguration getCustomConfig() {
+        return this.customConfig;
+    }
+
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "custom.yml");
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("custom.yml", false);
+         }
+
+        customConfig = new YamlConfiguration();
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            ((Throwable) e).printStackTrace();
+        }
+        /* User Edit:
+            Instead of the above Try/Catch, you can also use
+            YamlConfiguration.loadConfiguration(customConfigFile)
+        */
+    }
 
     @Override
     public void onDisable() {
         MessageUtils.sendMessage(getServer().getConsoleSender(), "Plugin disabled.");
-    }
-
-    public void loadConfig() {
-        getConfig().options().copyDefaults(true); // Copie les valeurs par défaut depuis config.yml
-        saveDefaultConfig(); // Sauvegarde la config si elle n'existe pas encore
-
-        this.debug = getConfig().getBoolean("debug", false); // Charge la valeur de "debug", false par défaut si non définie
     }
 }
